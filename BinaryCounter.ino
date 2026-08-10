1d8621603bd5cb73a91515d6b7d8d77ef4dc68b4
ChazHanda <75233346+ChazHanda@users.noreply.github.com>
2026-08-10 16:47:05 -0700

    Updated BinaryCounter.ino

--- BinaryCounter.ino
+++ BinaryCounter.ino
@@ -2,6 +2,7 @@ const int bit[] = {0, 2, 4, 6, 8, 10};
 const int bitCount = 6;
 
 const int button0 = 22;
+const int button1 = 21;
 int count = 0;
 
 void setup() {
@@ -9,26 +10,43 @@ void setup() {
     pinMode(bit[i], OUTPUT);
   }
 
-  pinMode(button0, INPUT);
+  pinMode(button0, INPUT_PULLUP);
+  pinMode(button1, INPUT_PULLUP);
+
+  pinMode(LED_BUILTIN, OUTPUT);
 
 }
 
 void loop() {
 
-  if (digitalRead(button0) == HIGH) { //if button is pressed
-    count++;
-    if (count > 63) {
+  if ((digitalRead(button0) == LOW) ^ (digitalRead(button1) == LOW)) { //if a single button is pressed
+
+    if (digitalRead(button0) == LOW) {  //Button0 is pressed
+      count++;
+      if (count > 63) {
       count = 0;
+      }
+    } else {  //Button1 is pressed
+      count--;
+      if (count < 0) {
+        count = 63;
+      }
     }
 
     for (int i = 0; i < bitCount; i++) {  //toggle LEDs based on count
       if (bitRead(count, i)) {
-        digitalWrite(bit[i], HIGH)
+        digitalWrite(bit[i], HIGH);
       } else {
-        digitalWrite(bit[i], LOW)
+        digitalWrite(bit[i], LOW);
       }
     }
   }
 
+  if (digitalRead(LED_BUILTIN) == LOW) {  //toggle built in LED
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  
   delay(500);
 }
